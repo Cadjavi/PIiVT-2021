ac58747bc2d6ef25374c62f86cfc26f87c485bf2
Aleksa Cadjo <aleksa.cadjo.16@singimail.rs>
2021-05-24 15:14:11 +0200

    Implement a middleware to verify tokens in req

--- 03-back-end/src/components/category/router.ts
+++ 03-back-end/src/components/category/router.ts
@@ -3,34 +3,40 @@ import CategoryService from "./service";
 import CategoryController from "./controller";
 import IApplicationResourses from "../../common/IApplicationResourses.interface";
 import IRouter from "../../common/IRouter.interface";
+import AuthMiddleware from "../../middleware/auth.middleware";
 
 export default class CategoryRouter implements IRouter {
   public setupRoutes(
     application: express.Application,
     resourses: IApplicationResourses
   ) {
-    
     const categoryController: CategoryController = new CategoryController(
       resourses
     );
 
     application.get(
       "/category",
+      AuthMiddleware.getVerifier("administrator", "user"),
       categoryController.getAll.bind(categoryController)
     );
     application.get(
       "/category/:id",
+      AuthMiddleware.getVerifier("administrator", "user"),
       categoryController.getById.bind(categoryController)
     );
     application.post(
       "/category",
+      AuthMiddleware.getVerifier("administrator"),
       categoryController.add.bind(categoryController)
     );
     application.put(
       "/category/:id",
+      AuthMiddleware.getVerifier("administrator"),
       categoryController.edit.bind(categoryController)
     );
-    application.delete("/category/:id",
+    application.delete(
+      "/category/:id",
+      AuthMiddleware.getVerifier("administrator"),
       categoryController.deleteById.bind(categoryController)
     );
   }
