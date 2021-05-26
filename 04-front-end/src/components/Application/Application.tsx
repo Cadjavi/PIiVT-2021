4c8cb7322528f58c704c48427af0062fc6a89f39
Aleksa Cadjo <aleksa.cadjo.16@singimail.rs>
2021-05-26 15:43:57 +0200

    Create a component that uses routes with matched parameters

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import TopMenu from '../TopMenu/TopMenu';
-import './Application.sass';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomePage from '../HomePage/HomePage';
-import CategoryPage from '../CategoryPage/CategoryPage';
-import ContactPage from '../ContactPage/ContactPage';
+import React from "react";
+import { Container } from "react-bootstrap";
+import TopMenu from "../TopMenu/TopMenu";
+import "./Application.sass";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "../HomePage/HomePage";
+import CategoryPage from "../CategoryPage/CategoryPage";
+import ContactPage from "../ContactPage/ContactPage";
 
-export default function Application() {
+export default function Application(props: any) {
   return (
     <BrowserRouter>
       <Container className="Application">
-        <div className="Application-header">
-          Front-end aplikacije
-        </div>
+        <div className="Application-header">Front-end aplikacije</div>
 
         <TopMenu />
 
         <div className="Application-body">
           <Switch>
-            <Route exact path="/" component={ HomePage } />
+            <Route exact path="/" component={HomePage} />
 
-            <Route path="/category/" component={ CategoryPage } />
+            <Route path="/category/:cid?" component={CategoryPage} />
 
             <Route path="/contact">
               <ContactPage
                 title="Our location in Belgrade"
                 address="Danijelova 32, 11010 Beograd, Srbija"
-                phone="+381 11 30 94 094" />
+                phone="+381 11 30 94 094"
+              />
             </Route>
 
-            <Route path="/profile">
-              My profile
-            </Route>
+            <Route path="/profile">My profile</Route>
           </Switch>
         </div>
 
-        <div>
-            &copy; 2021...
-        </div>
+        <div>&copy; 2021...</div>
       </Container>
     </BrowserRouter>
   );
--- 04-front-end/src/components/BasePage/BasePage.tsx
+++ 04-front-end/src/components/BasePage/BasePage.tsx
@@ -1,40 +1,47 @@
 /* eslint-disable @typescript-eslint/no-useless-constructor */
 import React from "react";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 
 class BasePageProperties {
-    sidebar?: JSX.Element = undefined
+  sidebar?: JSX.Element = undefined;
+  match?: any;
 }
 
 export { BasePageProperties };
 
-export default abstract class BasePage<Properties extends BasePageProperties> extends React.Component<Properties> {
-    constructor(props: Properties) {
-        super(props);
-    }
+export default abstract class BasePage<
+  Properties extends BasePageProperties
+> extends React.Component<Properties> {
+  constructor(props: Properties) {
+    super(props);
+  }
 
-    render() {
-        const sidebarSizeOnMd = this.props.sidebar ? 3 : 0;
-        const sidebarSizeOnLg = this.props.sidebar ? 4 : 0;
+  render() {
+    const sidebarSizeOnMd = this.props.sidebar ? 3 : 0;
+    const sidebarSizeOnLg = this.props.sidebar ? 4 : 0;
 
-        return (
-            <Row className="page-holder">
-                <Col className="page-body"
-                     sm={ 12 }
-                     md={ 12 - sidebarSizeOnMd }
-                     lg={ 12 - sidebarSizeOnLg }>
-                    { this.renderMain() }
-                </Col>
+    return (
+      <Row className="page-holder">
+        <Col
+          className="page-body"
+          sm={12}
+          md={12 - sidebarSizeOnMd}
+          lg={12 - sidebarSizeOnLg}
+        >
+          {this.renderMain()}
+        </Col>
 
-                <Col className="page-sidebar"
-                    sm={ 12 }
-                    md={ sidebarSizeOnMd }
-                    lg={ sidebarSizeOnLg }>
-                    { this.props.sidebar }
-                </Col>
-            </Row>
-        );
-    }
+        <Col
+          className="page-sidebar"
+          sm={12}
+          md={sidebarSizeOnMd}
+          lg={sidebarSizeOnLg}
+        >
+          {this.props.sidebar}
+        </Col>
+      </Row>
+    );
+  }
 
-    abstract renderMain(): JSX.Element;
+  abstract renderMain(): JSX.Element;
 }
