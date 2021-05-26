08394a1f94b4221eeccb1a37d3073dd70d1f8a19
Aleksa Cadjo <aleksa.cadjo.16@singimail.rs>
2021-05-26 14:38:32 +0200

    Implement BasePage

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import "./Application.sass";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import TopMenu from '../TopMenu/TopMenu';
+import './Application.sass';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from '../HomePage/HomePage';
+import CategoryPage from '../CategoryPage/CategoryPage';
+import ContactPage from '../ContactPage/ContactPage';
 
 export default function Application() {
   return (
-    <div className="Application container">
-      <div className="Application-header">
-        Front-end aplikacije
-      </div>
-      <p>
-        <button className="btn btn-primary">
-          Log in
-        </button>
-      </p>
-    </div>
+    <BrowserRouter>
+      <Container className="Application">
+        <div className="Application-header">
+          Front-end aplikacije
+        </div>
+
+        <TopMenu />
+
+        <div className="Application-body">
+          <Switch>
+            <Route exact path="/" component={ HomePage } />
+
+            <Route path="/category/" component={ CategoryPage } />
+
+            <Route path="/contact">
+              <ContactPage
+                title="Our location in Belgrade"
+                address="Danijelova 32, 11010 Beograd, Srbija"
+                phone="+381 11 30 94 094" />
+            </Route>
+
+            <Route path="/profile">
+              My profile
+            </Route>
+          </Switch>
+        </div>
+
+        <div>
+            &copy; 2021...
+        </div>
+      </Container>
+    </BrowserRouter>
   );
 }
